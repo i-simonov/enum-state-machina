b516333779cb8be7a96da293d41318c5e01ed017
i_simonov <simonov88.ivan@gmail.com>
2023-09-12 17:44:03 -0300

    foe review

--- src/main/java/com/example/enumstatemachina/StatusEnum.java
+++ src/main/java/com/example/enumstatemachina/StatusEnum.java
@@ -1,5 +1,6 @@
 package com.example.enumstatemachina;
 
+
 public enum StatusEnum {
     RED
             {
--- src/main/java/com/example/enumstatemachina/StatusMachina.java
+++ src/main/java/com/example/enumstatemachina/StatusMachina.java
@@ -1,5 +1,6 @@
 package com.example.enumstatemachina;
 
+
 import org.springframework.stereotype.Component;
 
 @Component
